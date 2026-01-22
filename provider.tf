27c2a348129368301fd0b84837a63dd1bd8505fa
YPanda207 <ypanday207@gmail.com>
2026-01-22 00:26:54 -0800

    Actual provider

--- provider.tf
+++ provider.tf
@@ -1,62 +1,63 @@
-provider "aws" {
-  # 👇 Dummy credentials so the provider stops looking at EC2 metadata / real accounts
-  access_key = "dummy"
-  secret_key = "dummy"
-
-  # 👇 Turn off all real AWS validation calls
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  default_tags {
-    tags = {
-      yp_tagId       = var.tag_id
-      yp_environment = var.yp_environment
-    }
-  }
-}
-terraform {
-  required_version = ">= 1.13.5"
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  backend "local" {
-    path = "state/common-terraform.tfstate"
-  }
-}
-
-# For actual AWS deploy
+# For local AWS deploy
 # provider "aws" {
-#   region = "us-east-1"
+#   # 👇 Dummy credentials so the provider stops looking at EC2 metadata / real accounts
+#   access_key = "dummy"
+#   secret_key = "dummy"
+
+#   # 👇 Turn off all real AWS validation calls
+#   skip_credentials_validation = true
+#   skip_metadata_api_check     = true
+#   skip_requesting_account_id  = true
 #   default_tags {
 #     tags = {
-#       yp-tagId       = var.tagId
-#       yp-environment = var.yp_environment
+#       yp_tagId       = var.tag_id
+#       yp_environment = var.yp_environment
 #     }
 #   }
 # }
-
 # terraform {
+#   required_version = ">= 1.13.5"
 #   required_providers {
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "~> 3.6"
+#     }
+#     local = {
+#       source  = "hashicorp/local"
+#       version = "~> 2.4"
+#     }
 #     aws = {
-#       source  = "harshicorp/aws"
+#       source  = "hashicorp/aws"
 #       version = "~> 5.0"
 #     }
 #   }
-#   backend "s3" {
-#     bucket  = "yp-bucket"
-#     key     = "state/terraform.tfstate"
-#     region  = "us-east-1"
-#     encrypt = true
+#   backend "local" {
+#     path = "state/common-terraform.tfstate"
 #   }
 # }
+
+# For actual AWS deploy
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      yp-tagId       = var.tagId
+      yp-environment = var.yp_environment
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "harshicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "yp-bucket"
+    key     = "state/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
